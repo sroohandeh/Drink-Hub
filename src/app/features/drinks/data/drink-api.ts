fc9281f8ddbf3e34037e2f1dbf1b9118af29b494
sroohandeh <s.roohandeh74@gmail.com>
2026-07-20 23:31:08 +0330

    set drink service

---
+++ src/app/features/drinks/data/drink-api.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { delay, map, Observable } from 'rxjs';
+import { Drink } from '../../../shared/models/drink';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DrinkApi {
+  private http = inject(HttpClient);
+  private baseUrl = 'assets/mock/drinks.json';
+
+  getDrinks(): Observable<Drink[]> {
+    return this.http.get<Drink[]>(this.baseUrl).pipe(
+      delay(500),
+      map((drinks) => drinks ?? []),
+    );
+  }
+
+  getDrinkById(id: string): Observable<Drink> {
+    return this.http.get<Drink[]>(this.baseUrl).pipe(
+      map((drinks) => {
+        const found = drinks.find((drink) => drink.id === id);
+        if (!found) {
+          throw new Error('Drink not found');
+        }
+        return found;
+      }),
+    );
+  }
+}
