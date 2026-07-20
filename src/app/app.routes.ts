aad94e6e81d9ecf76d135df92579e140cc907d4e
sroohandeh <s.roohandeh74@gmail.com>
2026-07-20 23:30:00 +0330

    set routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './core/layout/main-layout/main-layout';
+import { DrinkList } from './features/drinks/drink-list/drink-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: '', redirectTo: 'drinks', pathMatch: 'full' },
+      {
+        path: 'drinks',
+        component: DrinkList,
+      },
+    ],
+  },
+];
