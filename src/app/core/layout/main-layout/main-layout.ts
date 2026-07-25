497568f86a3744a545991295efa9624d3729fa7c
sroohandeh <s.roohandeh74@gmail.com>
2026-07-26 00:36:47 +0330

    set login mode in main layout

--- src/app/core/layout/main-layout/main-layout.ts
+++ src/app/core/layout/main-layout/main-layout.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { Token } from '../../services/token';
+import { User } from '../../../features/auth/data/auth.models';
 
 @Component({
   selector: 'app-main-layout',
@@ -8,5 +10,12 @@ import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
   styleUrl: './main-layout.css',
 })
 export class MainLayout {
+  tokenService = inject(Token);
   year = new Date().getFullYear();
+  isLoggedIn = !!localStorage.getItem('pdh_token');
+  user: User | null = this.tokenService.getUser();
+
+  constructor() {
+    console.log('user',this.user);
+  }
 }
